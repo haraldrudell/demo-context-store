23731a2f904409eca807019ce7d2d0c74dfec2c5
Harald Rudell <harald@attiresoft.com>
2017-08-27 16:07:40 -0700

    availabilitymanager

---
+++ cli/availabilitymanager/src/Process.js
@@ -0,0 +1,43 @@
+/*
+© 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
+This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
+*/
+class Process {
+  static instantiate = args => (async () => {
+    const options = args || false
+
+    if (options.hasOwnProperty('require') &&
+      (!options.require ||
+        options.require.main !== options.module)) return
+
+    const construct = typeof options === 'function' ? options : options.construct
+    if (typeof construct !== 'function') throw new Error('No constructor: options or options.construct not function')
+    const instance = new construct(options.options)
+
+    const method = String(options.method || '')
+    if (method) {
+      if (typeof instance[method] !== 'function') throw new Error(`method ${method} is not function`)
+      instance[method](options.methodArg)
+    }
+
+    const asyncMethod = String(options.async || '')
+    if (asyncMethod) {
+      if (typeof instance[asyncMethod] !== 'function') throw new Error(`method ${asyncMethod} is not function`)
+      await instance[asyncMethod](options.asyncArg)
+    }
+  })().catch(Process.errorHandler)
+
+  static async exit() {
+    process.exit()
+  }
+
+  static errorHandler(e) {
+    console.error('Process.errorHandler')
+    if (!(e instanceof Error)) e = new Error(util.format('Error was value: ', e))
+    console.error(e)
+    console.error(new Error('Process.errorHandler invokation').stack)
+    process.exit(1)
+  }
+}
+
+export default Process.instantiate
