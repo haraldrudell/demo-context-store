62442fedb8e26dfad45c739c414b66602236d65e
Harald Rudell <harald@attiresoft.com>
2017-09-13 23:12:26 -0700

    cross-spawn-async

---
+++ packages/cross-spawn-async/src/spawn.js
@@ -0,0 +1,19 @@
+/*
+© 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
+This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
+*/
+import crossSpawn from 'cross-spawn'
+
+export default async function spawn({cmd, args, options}) {
+  return new Promise((resolve, reject) => crossSpawn(cmd, args, {...options, stdio: ['ignore', 'inherit', 'inherit']})
+    .once('close', (status, signal) => {
+      if (status === 0 && !signal) resolve(status)
+      else {
+        let msg = `status code: ${status}`
+        if (signal) msg += ` signal: ${signal}`
+        msg += ` '${cmd} ${args.join(' ')}'`
+        reject(new Error(msg))
+      }
+    }).on('error', reject)
+  )
+}
