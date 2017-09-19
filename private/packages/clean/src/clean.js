7faa756c09a89d6ab8de5e03e1f6f544f4f8d0ac
Harald Rudell <harald@attiresoft.com>
2017-09-19 02:38:47 -0700

    clean ported updated clean - still works

--- private/packages/clean/src/clean.js
+++ private/packages/clean/src/clean.js
@@ -1,31 +1,24 @@
 /*
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
-This source code is licensed under license found in the LICENSE file in the root directory of this source tree.
+All rights reserved.
 */
 import fs from 'fs-extra'
 import path from 'path'
 
-export default list => clean(list).catch(errorHandler)
-
-async function clean(args) {
+export default async function clean(args) {
   if (typeof args === 'string') args = [args]
-  else if (!Array.isArray(args)) throw new Error('clean: package.json cleans field not non-empty string or array')
-  args.forEach((s, index) => {
+  else if (!Array.isArray(args) || !args.length) throw new Error('clean: argument not non-empty string or array')
+  for (let [index, s] of args) {
     const st = typeof s
     if (st !== 'string' || !s) throw new Error(`clean: index ${index}: not non-empty string: ${st}`)
-  })
+  }
   console.log(`clean: ${args.join(' ')}…`)
 
-  const projectDir = process.cwd()
-  await Promise.all(args.map(s => removeIfExist(path.join(projectDir, s))))
+  const projectDir = fs.realpathSync(process.cwd()) // project directory without symlinks
+  await Promise.all(args.map(s => removeIfExist(path.resolve(projectDir, s))))
   console.log('clean completed successfully.')
 }
 
 async function removeIfExist(p) {
   if (await fs.exists(p)) await fs.remove(p)
-}
-
-function errorHandler(e) {
-  console.error(e instanceof Error ? e.message : e)
-  process.exit(1)
 }
