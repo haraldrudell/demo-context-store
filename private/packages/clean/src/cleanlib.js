f6a68d5b529a14df4d44f4aad599ced2185b83d1
Harald Rudell <harald@attiresoft.com>
2017-09-19 02:34:05 -0700

    clean own rollup.config.js works

--- private/packages/clean/src/cleanlib.js
+++ private/packages/clean/src/cleanlib.js
@@ -2,5 +2,5 @@
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
 This source code is licensed under license found in the LICENSE file in the root directory of this source tree.
 */
-export {default as default} from './clean'
+export * from './clean'
 export {default as cleanPlugin} from './cleanPlugin'
