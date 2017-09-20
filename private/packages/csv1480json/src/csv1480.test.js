cf5acf16d4bebb53768e29cf622e5aff1e259290
Harald Rudell <harald@attiresoft.com>
2017-09-20 09:44:04 -0700

    androidmanager csv1480json test executables

--- private/packages/csv1480json/src/csv1480.test.js
+++ private/packages/csv1480json/src/csv1480.test.js
@@ -2,17 +2,23 @@
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
 All rights reserved.
 */
-import {EventEmitter} from 'events'
 import {spawn} from 'child_process'
 
-/* an emitted error is propely handled as an error
-test('Emit Error', async () => {
-  new EventEmitter().emit('error', new Error('x'))
-})
-*/
-
-test('run executable', async () => { // ./bin/csv1480json, ['--help']
-  await  new Promise((resolve, reject) => spawn('bin/csv1480json', {stdio: ['ignore', 'inherit', 'inherit']})
-    .once('close', (status, signal) => status === 0 && !signal && resolve() || reject(new Error(`status: ${status} signal: ${signal}`)))
-  )
+test('csv1480json should convert 1 to json', async () => {
+  const expected = '{1: "1"}\n'
+  const actual = await  new Promise((resolve, reject) => {
+    let stderr = ''
+    let stdout = ''
+    const cp = spawn('bin/csv1480json', {stdio: 'pipe'})
+      .once('close', (status, signal) => {
+        if (status === 0 && !signal) {
+          if (!stderr) resolve(stdout)
+          else reject(new Error(`csv1480json echo to stderr: '${stderr}' stdout: '${stdout}'`))
+        } else reject(new Error(`status: ${status} signal: ${signal}`))
+      }).on('error', reject)
+    cp.stdout.on('data', s => stdout += s).on('error', reject).setEncoding('utf8')
+    cp.stderr.on('data', s => stderr += s).on('error', reject).setEncoding('utf8')
+    cp.stdin.end('1\n')
+  })
+  expect(actual).toEqual(expected)
 })
