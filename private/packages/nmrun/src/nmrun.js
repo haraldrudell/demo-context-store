a2e762e02a0295e4677dcf1ae9b403052bfa9aee
Harald Rudell <harald@attiresoft.com>
2017-10-22 13:50:18 -0700

    nmrun extract argv

--- private/packages/nmrun/src/nmrun.js
+++ private/packages/nmrun/src/nmrun.js
@@ -6,15 +6,14 @@ import Runner from './Runner'
 
 import fs from 'fs-extra'
 
-run().catch(errorHandler)
+run({args: process.argv.slice(2), dir: process.cwd()}).catch(errorHandler)
 
-async function run() {
+async function run({args, dir}) {
   const m = 'nmrun'
 
-  const args = process.argv.slice(2)
   const command = getNonEmpty(args[0], `${m} command`)
   args.splice(0, 1)
-  const dir = await fs.realpath(process.cwd())
+  const dir = await fs.realpath(dir)
   return new Runner().run({dir, command, args})
 }
 
