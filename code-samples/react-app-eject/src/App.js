9ee95d38a379ae262c9ebfed9a1bda3fdf9e8cf1
Harald Rudell <harald@attiresoft.com>
2017-12-21 21:01:24 -0800

    react-app-eject @bound

--- code-samples/react-app-eject/src/App.js
+++ code-samples/react-app-eject/src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {bound} from 'class-bind'
 
 class App extends Component {
-  render() {
+  constructor(...args) {
+    super(...args)
+    const f = this.boundTest
+    f(this)
+}
+
+@bound boundTest(properThis) {
+    console.log(this === properThis ? '@bound annotation working correctly' : 'function was not bound')
+}
+
+render() {
     return (
       <div className="App">
         <header className="App-header">
