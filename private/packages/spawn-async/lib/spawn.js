d558d83ad8f8a0ddb50cc5983953e78c57062ef0
Harald Rudell <harald@attiresoft.com>
2017-12-29 14:51:11 -0800

    spawn-async: returns child_process object

--- private/packages/spawn-async/lib/spawn.js
+++ private/packages/spawn-async/lib/spawn.js
@@ -5,6 +5,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var _regeneratorRuntime = _interopDefault(require('babel-runtime/regenerator'));
+var _Object$assign = _interopDefault(require('babel-runtime/core-js/object/assign'));
 var _extends = _interopDefault(require('babel-runtime/helpers/extends'));
 var _Promise = _interopDefault(require('babel-runtime/core-js/promise'));
 var _asyncToGenerator = _interopDefault(require('babel-runtime/helpers/asyncToGenerator'));
@@ -16,20 +17,20 @@ This source code is licensed under the ISC-style license found in the LICENSE fi
 */
 var spawn$1 = (function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee(o) {
-    var _ref2, cmd, _ref2$args, args, options;
+    var _ref2, cmd, _ref2$args, args, options, cp;
 
     return _regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _ref2 = o || false, cmd = _ref2.cmd, _ref2$args = _ref2.args, args = _ref2$args === undefined ? [] : _ref2$args, options = _ref2.options;
+            _ref2 = o || false, cmd = _ref2.cmd, _ref2$args = _ref2.args, args = _ref2$args === undefined ? [] : _ref2$args, options = _ref2.options, cp = _ref2.cp;
 
             if (!(typeof cmd !== 'string' || !cmd)) {
               _context.next = 3;
               break;
             }
 
-            throw new Error('spawn-async: cmd not non-empty string');
+            throw new Error('spawn-async: command not non-empty string');
 
           case 3:
             if (Array.isArray(args)) {
@@ -41,14 +42,17 @@ var spawn$1 = (function () {
 
           case 5:
             return _context.abrupt('return', new _Promise(function (resolve, reject) {
-              debugger;return child_process.spawn(cmd, args, _extends({ stdio: ['ignore', 'inherit', 'inherit'] }, options)).once('close', function (status, signal) {
+              var c = child_process.spawn(cmd, args, _extends({ stdio: ['ignore', 'inherit', 'inherit'] }, options)).once('close', function (status, signal) {
                 if (status === 0 && !signal) resolve(status);else {
                   var msg = 'status code: ' + status;
                   if (signal) msg += ' signal: ' + signal;
                   msg += ' \'' + cmd + ' ' + args.join(' ') + '\'';
-                  reject(new Error(msg));
+                  var e = new Error(msg);
+                  _Object$assign(e, { status: status, signal: signal });
+                  reject(e);
                 }
               }).on('error', reject);
+              if (cp) cp.cp = c;
             }));
 
           case 6:
--- private/packages/spawn-async/src/spawn.js
+++ private/packages/spawn-async/src/spawn.js
@@ -5,18 +5,23 @@ This source code is licensed under the ISC-style license found in the LICENSE fi
 import {spawn as cspawn} from 'child_process'
 
 export default async function spawn(o) {
-  const {cmd, args = [], options} = o || false
+  const {cmd, args = [], options, cp} = o || false
   if (typeof cmd !== 'string' || !cmd) throw new Error('spawn-async: command not non-empty string')
   if (!Array.isArray(args)) throw new Error('spawn-async: args not array')
-  return new Promise((resolve, reject) => {debugger; return cspawn(cmd, args, {stdio: ['ignore', 'inherit', 'inherit'], ...options})
-    .once('close', (status, signal) => {
-      if (status === 0 && !signal) resolve(status)
-      else {
-        let msg = `status code: ${status}`
-        if (signal) msg += ` signal: ${signal}`
-        msg += ` '${cmd} ${args.join(' ')}'`
-        reject(new Error(msg))
-      }
-    }).on('error', reject)
-    }  )
+
+  return new Promise((resolve, reject) => {
+    const c = cspawn(cmd, args, {stdio: ['ignore', 'inherit', 'inherit'], ...options})
+      .once('close', (status, signal) => {
+        if (status === 0 && !signal) resolve(status)
+        else {
+          let msg = `status code: ${status}`
+          if (signal) msg += ` signal: ${signal}`
+          msg += ` '${cmd} ${args.join(' ')}'`
+          const e = new Error(msg)
+          Object.assign(e, {status, signal})
+          reject(e)
+        }
+      }).on('error', reject)
+      if (cp) cp.cp = c
+    })
 }
