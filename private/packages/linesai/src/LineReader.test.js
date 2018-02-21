03535e638f4427bef1ddd13111691e1f09f5fb94
Harald Rudell <harald@attiresoft.com>
2018-02-21 00:18:00 -0800

    LineReader refactored for sudodns

--- private/packages/linesai/src/LineReader.test.js
+++ private/packages/linesai/src/LineReader.test.js
@@ -2,9 +2,42 @@
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
 This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
 */
-import LineReader from './LineReader'
+//import LineReader from './LineReader'
 
 import {Readable} from 'stream'
+import pjson from '../package.json'
+
+import fs from 'fs-extra'
+
+import path from 'path'
+import childProcess from 'child_process'
+const {ChildProcess, execSync} = childProcess
+
+const {main} = Object(pjson)
+if (!main || typeof main !== 'string') throw new Error(`package.json main not non-empty string`)
+
+const projectDir = path.resolve()
+const executableAbsolute = path.resolve(projectDir, main)
+const srcDir = path.resolve('src')
+const executableRelative = path.relative(srcDir, executableAbsolute)
+
+const exportName = 'LineReader'
+let LineReader
+
+test('yarn build should have completed', () => {
+  let executableExports
+  let e
+  try {
+    executableExports = require(executableRelative)
+  } catch (ee) {
+    e = ee
+  }
+  if (e) expect(`failed to require: '${main}': Error: ${e.message}`).toBeNull()
+  expect(typeof executableExports).toBe('object')
+  const theExport = executableExports[exportName]
+  expect(typeof theExport).toBe('function')
+  LineReader = theExport
+})
 
 test('extend works', async () => {
   const lr = new LineReader(new Readable())
--- private/packages/linesai/src/StringSplitter.test.js
+++ private/packages/linesai/src/StringSplitter.test.js
@@ -2,7 +2,40 @@
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
 This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
 */
-import StringSplitter from './StringSplitter'
+//import StringSplitter from './StringSplitter'
+import pjson from '../package.json'
+
+import fs from 'fs-extra'
+
+import path from 'path'
+import childProcess from 'child_process'
+const {ChildProcess, execSync} = childProcess
+
+const {main} = Object(pjson)
+if (!main || typeof main !== 'string') throw new Error(`package.json main not non-empty string`)
+
+const projectDir = path.resolve()
+const executableAbsolute = path.resolve(projectDir, main)
+const srcDir = path.resolve('src')
+const executableRelative = path.relative(srcDir, executableAbsolute)
+
+const exportName = 'StringSplitter'
+let StringSplitter
+
+test('yarn build should have completed', () => {
+  let executableExports
+  let e
+  try {
+    executableExports = require(executableRelative)
+  } catch (ee) {
+    e = ee
+  }
+  if (e) expect(`failed to require: '${main}': Error: ${e.message}`).toBeNull()
+  expect(typeof executableExports).toBe('object')
+  const theExport = executableExports[exportName]
+  expect(typeof theExport).toBe('function')
+  StringSplitter = theExport
+})
 
 test('Empty string immediately ends', async () => {
   const fetchValues = ['', false]
