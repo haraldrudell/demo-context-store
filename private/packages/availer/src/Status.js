d43e886fba96217757385e2760a88967196520d4
Harald Rudell <harald@attiresoft.com>
2018-02-23 15:48:40 -0800

    iptables research

--- private/packages/availer/src/Status.js
+++ private/packages/availer/src/Status.js
@@ -126,28 +126,7 @@ export default class Status {
 
   getISOTime = Status.getISOTime
 
-  static getISOTime(timeval, withMs, noTz) {
-    if (!(timeval >= 0)) throw new Error(`Status.getISOTime: timeval not number >= 0: ${typeof timeval}`)
-    const tzAddMin = -new Date().getTimezoneOffset()
-    const s = new Date(timeval + tzAddMin * 6e4).toISOString() // 2017-09-01T19:02:46.322Z
-    const endIndex = !withMs ? 19 : 23
-    const day = s.substring(0, 10) // 2017-09-01
-    const time = s.substring(11, endIndex) // 19:02:46 [.322]
-    let result = `${day} ${time}`
-
-    const withTz = !noTz
-    if (withTz) {
-      const tzMin = Math.abs(tzAddMin)
-      const tzString = // '-07'
-        `${tzAddMin < 0 ? '-' : '+'}` +
-        `${tzMin < 600 ? '0' : ''}` +
-        `${Math.floor(tzMin / 60)}` +
-        `${tzMin % 60 > 0 ? Math.floor(tzMin % 60) : ''}`
-      result += tzString
-    }
-
-    return result
-    }
+  //getISOTime es2049lib
 }
 
 export const getISOTime = Status.getISOTime
