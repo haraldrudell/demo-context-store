60157eb91fd812d23fe24f00d7a0e22f95dc1cec
Harald Rudell <harald.rudell@gmail.com>
2018-12-05 03:37:39 -0800

    babelenv: imports, not texts

--- forks/ecmascript2049/workspace/packages/es2049scripts/src/babelenv.mjs
+++ forks/ecmascript2049/workspace/packages/es2049scripts/src/babelenv.mjs
@@ -2,17 +2,19 @@
 © 2017-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
 This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
 */
+import presetEnv from '@babel/preset-env'
 import stage0Preset from './stage0Preset'
+
 export default {
   development: {
     babelrc: false,
     sourceMaps: true,
-    presets: [['@babel/preset-env', {targets: {node: '8.5'}}], stage0Preset],
+    presets: [[presetEnv, {targets: {node: '8.5'}}], stage0Preset],
   },
   active: {
       babelrc: false,
       sourceMaps: true,
-      presets: [['@babel/preset-env', {targets: {node: '8.9.4'}}], stage0Preset],
+      presets: [[presetEnv, {targets: {node: '8.9.4'}}], stage0Preset],
   },
   current: {
     babelrc: false,
@@ -27,6 +29,6 @@ export default {
   rollup: {
     babelrc: false,
     sourceMaps: true,
-    presets: [['@babel/preset-env', {targets: {node: '8.5'}}], stage0Preset],
+    presets: [[presetEnv, {targets: {node: '8.5'}, modules: false}], stage0Preset],
   },
 }
