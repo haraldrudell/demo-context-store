01d2edf7cbf5636fe5b73055725ecbef84447b91
Harald Rudell <harald.rudell@gmail.com>
2018-12-10 20:12:07 -0800

    connect2

--- src/allstore/connect.js
+++ src/allstore/connect.js
@@ -3,28 +3,28 @@
 This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
 */
 import React, { Component, PureComponent } from 'react'
-import { storeContext} from './Store'
+import { storeContext } from './Store'
 // TODO 181210 haru: simplify this API
-export const connect = (mapStateToProps, options) => ConnectedComponent => getConnector({mapStateToProps, ConnectedComponent, options})
+export const connect = (mapStateToProps, options) => ConnectedComponent => connect2(mapStateToProps, ConnectedComponent, options)
 
-function getConnector({mapStateToProps, ConnectedComponent, options}) {
+export function connect2(mapStateToProps = store => store, ConnectedComponent, options) {
   if (!options) options = {}
   const displayName = String(options.displayName || ConnectedComponent.displayName || ConnectedComponent.name || 'Connect')
   const pure = options.pure !== false
-  if (mapStateToProps === undefined) mapStateToProps = store => store
-  else if (typeof mapStateToProps !== 'function') throw new Error('connect: mapStateToProps not function')
+  if (typeof mapStateToProps !== 'function') throw new Error('connect: mapStateToProps not function')
   class Connect extends (pure ? PureComponent : Component) {
     getSelectors = state => Object(mapStateToProps(state, this.props))
     componentDidMount = () => (this.subscription = this.context.subscribe(this)) + (this.lastProps = this.getSelectors(this.context.getState()))
     componentWillUnmount = () => this.subscription.unsubscribe()
     next(state) {
-      const {lastProps} = this
-      const newProps = this.getSelectors(state)
-      const list = Object.entries(newProps)
-      if (Object.keys(lastProps).length === list.length && list.every(([key, value]) => value === lastProps[key])) return // no change
-      this.lastProps = newProps
-      const a = this.a ? ++this.a : (this.a = 1)
-      this.setState({a})
+      if (pure) {
+        const {lastProps} = this
+        const newProps = this.getSelectors(state)
+        const list = Object.entries(newProps)
+        if (Object.keys(lastProps).length === list.length && list.every(([key, value]) => value === lastProps[key])) return // no change
+        this.lastProps = newProps
+      }
+      this.setState({a: this.a ? ++this.a : (this.a = 1)})
     }
 
     render() {
