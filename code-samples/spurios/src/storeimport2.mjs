5b694536c733a23b533146a1e2a10b084b559cd8
Harald Rudell <harald.rudell@gmail.com>
2018-12-18 06:13:20 -0800

    test export of static object for allstore

---
+++ code-samples/spurios/src/storeimport2.mjs
@@ -0,0 +1,8 @@
+/*
+© 2018-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
+All rights reserved.
+*/
+import {store} from './store'
+export function printStore() {
+  console.log('store:', store)
+}
