b422ec0fe76dcfef7938ac6dd383dbc91849c448
Harald Rudell <harald.rudell@gmail.com>
2018-12-30 00:20:56 -0800

    demo-context-store moved to subtree

--- code-samples/demo-context-store/src/allstore/context.js
+++
@@ -1,8 +0,0 @@
-/*
-© 2018-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
-All rights reserved.
-*/
-import { createContext } from 'react'
-
-export const storeContext = createContext()
-export const {Provider: StoreProvider, Consumer: StoreConsumer} = storeContext
